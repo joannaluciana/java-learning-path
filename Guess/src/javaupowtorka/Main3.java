d3892fcfcb45fbabe3a31e27a6092994efcca5a7
Joanna <joannaluciana>
2021-01-31 04:59:50 +0100

    Array - string.

---
+++ Guess/src/javaupowtorka/Main3.java
@@ -0,0 +1,26 @@
+package javaupowtorka;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main3 {
+	
+	public static void main(String[] args) {
+		String[] names = new String [4];
+		
+		String[]names= {"Asia","Basia","Micha³","Zdzisia"};
+//		names[0]="Asia";
+//		names[1]="Basia";
+//		names[2]="Micha³";
+//		names[3]="Zdzisia";
+		
+		for (int i = 0; i<names.length; i++) {
+			System.out.println(names[i]);
+		}
+		
+		}
+		
+		
+	}
+
+	
