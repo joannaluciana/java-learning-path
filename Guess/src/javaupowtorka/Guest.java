41a238ea0254166322f669fc0cc4d4333794a39e
Joanna <joannaluciana>
2021-02-03 22:41:30 +0100

    Adding classes

---
+++ Guess/src/javaupowtorka/Guest.java
@@ -0,0 +1,46 @@
+package javaupowtorka;
+
+public class Guest {
+	private String name;
+	
+	private String meal;
+	private int phoneNumber;
+	private boolean isVegan;
+	
+	
+	
+	
+	public Guest(String name, String meal, int phoneNumber, boolean isVegan) {
+		super();
+		this.name = name;
+		this.meal = meal;
+		this.phoneNumber = phoneNumber;
+		this.isVegan = isVegan;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public String getMeal() {
+		return meal;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public boolean isVegan() {
+		return isVegan;
+	}
+	
+	
+	public void displayGuestInformation() {
+		System.out.println("Imiê" + name);
+		System.out.println("Imiê" + meal);
+		System.out.println("Imiê" + phoneNumber);
+		System.out.println("Imiê" + name);
+		String isVeganString = isVegan ? "tak": "nie";
+		System.out.println("Vege" + isVeganString);
+	}
+	
+
+}
--- Guess/src/javaupowtorka/Party.java
+++ Guess/src/javaupowtorka/Party.java
@@ -6,20 +6,36 @@ import java.util.Scanner;
 
 public class Party {
 	
-	private List<String> guests = new ArrayList<>();
+	private List<Guest> guests = new ArrayList<>();
 	Scanner scanner = new Scanner(System.in);
 	
 	public void addGuest () {
 		
 		System.out.println("Podaj imiê goœcia");
 		String name = scanner.nextLine();
-		guests.add(name);
+		
+		System.out.println("Podaj posi³ek dla Ciebie");
+		String meal= scanner.nextLine();
+		
+		System.out.println("Podaj numer do Ciebie");
+		int phoneNumber= Integer.valueOf(scanner.nextLine());
+		
+		System.out.println("Vege or not?");
+		String isVeganString= scanner.nextLine();
+		
+		boolean isVegan;
+		if (isVeganString.equals("Y")) {isVegan=true;}
+		else {isVegan=false;}
+		
+		Guest guest = new Guest(name,meal, phoneNumber, isVegan);
+		
+		guests.add(guest);
 		}
 	
 	
 
 	public void displayGuests() {
-		for(String guest:guests) {
+		for(Guest guest: guests) {
 			System.out.println(guest);
 		}
 	}
